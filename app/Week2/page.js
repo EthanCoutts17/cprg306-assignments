5e35c7262bb19068f5c2282449f518db3a78009f
EthanCoutts17 <ethankcoutts17@gmail.com>
2023-09-24 20:43:45 -0600

    lab 2 web dev

--- app/Week2/page.js
+++ app/Week2/page.js
@@ -1,7 +1,7 @@
 import MyComp from "../StudentInfo";
 
 
-export default function newPage(){
+export default function week2(){
                         return(
                         <main>
                              <h1>My Shopping List</h1>  
