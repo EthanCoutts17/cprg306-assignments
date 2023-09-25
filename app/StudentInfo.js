96ba0bd740999f376a03726d46d8a5d1751e6533
EthanCoutts17 <ethankcoutts17@gmail.com>
2023-09-24 20:22:39 -0600

    First changes

---
+++ app/StudentInfo.js
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+export default function Mycomp(){
+       return(
+       <div>
+                        <h2>Ethan Coutts</h2>
+                        <Link href = ""
+
+
+       </div>
+
+
+
+       )
+                     
+}
